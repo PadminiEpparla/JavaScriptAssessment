395286a73d97b315d4b1bb23d8918f4178d5899b
PadminiEpparla <88498024+PadminiEpparla@users.noreply.github.com>
2021-08-05 19:55:54 +0530

    Create q8.js

---
+++ q8.js
@@ -0,0 +1,18 @@
+const name = {
+    firstName: 'Philip',
+    lastName: 'Fry'
+};
+const details = {
+    job: 'Delivery Boy',
+    employer: 'Planet Express'
+};
+const character = Object.assign(name, details);
+
+console.log(character);
+
+//Output: {
+//firstName: 'Philip',
+// lastName: 'Fry',
+//job: 'Delivery Boy',
+//employer: 'Planet Express'
+}
